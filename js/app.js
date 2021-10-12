d699320ccca4f8aad5a23a20a4e51aa52405d64b
TheRedMine TheredMaths <75205990+TheRedMineTheRedMine@users.noreply.github.com>
2021-10-12 11:10:27 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// menu dans chaque pages
 const header = document.querySelector('header');
 
 const menu_toggler = document.querySelector('.menu_toggler');
@@ -6,6 +7,12 @@ const menu_carret_toggler =  menu_toggler.querySelector('i');
 const menu = header.querySelector('nav.menu');
 const circle_links = document.querySelector('.circle_links');
 
+// dans "home" uniquement
+// permet d'afficher les options d'un sujet
+const options_toggler = document.querySelector('.result .part1 .parameters');
+const options = document.querySelector('.result .part1 .options');
+
+// activer/désactiver le menu
 menu_toggler.addEventListener('click', () => {
     if (menu_carret_toggler.classList.contains('fa-bars')) {
         menu.classList.add('active');
@@ -17,7 +24,18 @@ menu_toggler.addEventListener('click', () => {
         menu.classList.remove('active');
         header.classList.remove('active');
         circle_links.classList.remove('active');
-        
+
         menu_carret_toggler.classList.replace('fa-times', 'fa-bars');
     }
+});
+
+// options (home)
+// document.addEventListener('click', () => {
+//     if (options.classList.contains('active')) {
+//         options.classList.remove('active');
+//     }
+// });
+
+options_toggler.addEventListener('click', () => {
+    options.classList.toggle('active');
 });
