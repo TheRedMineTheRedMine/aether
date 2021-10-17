eba5dc39c1fddb99296dea4b23953936f42e6559
TheRedMine TheredMaths <75205990+TheRedMineTheRedMine@users.noreply.github.com>
2021-10-17 13:43:53 +0200

    Create home.js

---
+++ js/home.js
@@ -0,0 +1,25 @@
+const topics = document.querySelectorAll('.results > .result .re_title');
+const box = document.querySelector('.box_newDiscuss');
+const options_toggler = document.querySelector('.result .part1 .parameters');
+const options = document.querySelector('.result .part1 .options');
+
+[].forEach.call(topics, el => {
+    el.addEventListener('click', () => {
+        window.location.replace(("../discuss?q=" + el.dataset.id))
+    })
+});
+
+document.querySelector('.pages .text').addEventListener('click', () => { box.classList.add('active') });
+box.querySelector('.fa-times').addEventListener('click', () => { box.classList.remove('active') });
+
+
+// options (home)
+// document.addEventListener('click', () => {
+//     if (options.classList.contains('active')) {
+//         options.classList.remove('active');
+//     }
+// });
+
+options_toggler.addEventListener('click', () => {
+    options.classList.toggle('active');
+});
