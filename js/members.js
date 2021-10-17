058223625ace2f2de2e48a0827bf14d399b0b66b
TheRedMine TheredMaths <75205990+TheRedMineTheRedMine@users.noreply.github.com>
2021-10-17 13:44:48 +0200

    Update members.js

--- js/members.js
+++ js/members.js
@@ -1,3 +1,5 @@
+const users = document.querySelectorAll('.sort > .member')
+
 [].forEach.call(users, el => {
     el.addEventListener('click', () => {
         window.location.replace('../profil?id=' + el.dataset.id);
